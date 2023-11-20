e4215eb7e346374ed9b932cf6282b442b447b426
Adam Ploshay <aploshay@iu.edu>
2023-11-20 07:06:11 -0500

    wip controller changes

--- app/controllers/sda_controller.rb
+++ app/controllers/sda_controller.rb
@@ -5,49 +5,60 @@ class SdaController < ApplicationController
 
   def show
     if user_signed_in? && current_ability.admin?
-      render plain: sda_url_for(params[:collection], "#{params[:object]}.#{params[:format]}"), status: 200
+      render plain: archive_url_for(params[:collection], "#{params[:object]}.#{params[:format]}"), status: 200
     else
       render plain: 'action unavailable', status: 403
     end
   end
 
-  def test_request
-    # 404
-    string = "http://localhost:8181/datacore/xxx9352841_home_banner.jpg"
-    filename = "not available"
-    # 200
-    string = "http://localhost:8181/datacore/nv9352841_home_banner.jpg"
-    filename = "home_banner.jpg"
-    # 503 - but only briefly
-    string = "http://localhost:8181/datacore/fq977t78v_quaternary_EastSouthBend_in_100k_readme.txt"
-    filename = "readme.txt"
-    # 200
-    string = "http://localhost:8181/datacore/nv9352841_home_banner.jpg"
-    filename = "home_banner.jpg"
+  def request_factory(method)
+    case method
+    when :get
+      Net::HTTP::Get
+    when
+      Net::HTTP::Head
+    else
+      nil
+    end
+  end
 
+  def archive_request(string: "http://localhost:8181/datacore/nv9352841_home_banner.jpg", filename: "home_banner.jpg", method: :head)
     uri = URI.parse(string)
-    request = Net::HTTP::Get.new(uri.request_uri)
-    user = "datacore"
-    pass = "c94c94854864a13605541daeba4f0b55"
-    request['Authorization'] = "#{user}:#{pass}"
+    request = request_factory(method).new(uri.request_uri)
+    request['Authorization'] = "#{Settings.archive_api.username}:#{Settings.archive_api.password}"
+    # TODO: add begin wrapper
     result = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http|
       http.request(request)
     }
-    case result.code
+    return result
+  end
+
+  def status_request(string: "http://localhost:8181/datacore/nv9352841_home_banner.jpg", filename: "home_banner.jpg")
+    archive_request(string: string, filename: filename, method: :head)
+  end
+
+  def file_request(string: "http://localhost:8181/datacore/nv9352841_home_banner.jpg", filename: "home_banner.jpg")
+    archive_request(string: string, filename: filename, method: :get)
+  end
+
+  def handle_file_request(string: "http://localhost:8181/datacore/nv9352841_home_banner.jpg", filename: "home_banner.jpg", code: nil)
+    result = file_request(string: string, filename: filename)
+    code = result.try(:code) || 'No response from file archiver service'
+    case code
       when '503'
-        render plain: '503'
+        redirect_to [main_app, FileSet.find('zs25x844t')], notice: 'File found in archives and requested for download'
       when '200'
         #render plain: result.body
         send_data result.body, filename: filename
       when '404'
-        render plain: '404'
+        redirect_to [main_app, FileSet.find('zs25x844t')], alert: 'File not found in archives'
       else
-        render plain: 'unknown result'
+        redirect_to [main_app, FileSet.find('zs25x844t')], alert: "Unexpected response from archives: #{code}"
     end
   end
 
   private
-    def sda_url_for(collection, object, version: 1)
-      Settings.sda_api.show % [collection, object, version]
+    def archive_url_for(collection, object, version: 1)
+      Settings.archive_api.show % [collection, object, version]
     end
 end
